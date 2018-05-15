58d806be09845aa9faab370ef787851f5cd545d3
HazyDayzMN <justin.hammond@optum.com>
2018-05-15 11:12:16 -0500

    updated your to youre

--- thegame/webroot/results/jjh-result.js
+++ thegame/webroot/results/jjh-result.js
@@ -1,5 +1,5 @@
 function Result() {
 	this.WIN  = "Looks like your WINdows are clean!!!";
-	this.LOSS = "Your lost in space :(";
+	this.LOSS = "Youre lost in space :(";
 	this.TIE  = "Your Tae Kwon Do needs some help";
 }
